2b1f9934c9e38ea3f3da33e22e90a50fc2aab3e3
JhonnySRocha22 <joaovictor200152@gmail.com>
2023-10-19 16:32:59 -0300

    Construindo o Menu

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//Selecionando o elemento (através da descendência)que acioinará o menu
+const botaoMenu = document.querySelector("nav h2");
+
+
+//Selecione a lista/menu através da classe
+const menu = document.querySelector(".menu");
+
+
+
+//Selecionando o link que está dentro do nav h2
+const textoBotao = botaoMenu.querySelector("a");
+
+
