7f878b5a9de70b93d853d08d92e810c4eebac0df
JhonnySRocha22 <joaovictor200152@gmail.com>
2023-10-25 15:00:41 -0300

    Contato - mensagens

--- js/contato.js
+++ js/contato.js
@@ -74,10 +74,46 @@ campoEndereco.value = dados.logradouro;
 campoEstado.value = dados.localidade;
 campoCidade.value = dados.uf;
 
-
-
 }
 
 
 
 });
+
+
+
+
+
+/* Peogramação do Formspree */
+
+
+
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: formulario.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Seus dados foram enviados!! Aguardo Retorno.";
+          formulario.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Deu Ruim!! Algo de errado não está certo!"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Deu Ruim!! Algo de errado não está certo!"
+      });
+    }
+    formulario.addEventListener("submit", handleSubmit)
